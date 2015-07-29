82cd90bafbb8ebd709f632785d5c90d8b4e155bb
Eugene Kovalev <seanchas@gmail.com>
2015-07-29 19:03:08 +0300

    Removed overthinking of provided type

--- lib/graphql/type/definable.rb
+++ lib/graphql/type/definable.rb
@@ -10,12 +10,6 @@ module GraphQL
           unless args.length == 0
             next_value = args.first
             raise GraphQL::Error::Type unless proc.call(next_value)
-            prev_value = instance_variable_get(:"@#{name}")
-            next_value = case prev_value
-            when Array  then prev_value.concat(next_value)
-            when Hash   then prev_value.merge(next_value)
-            else next_value
-            end
             instance_variable_set(:"@#{name}", next_value)
           end
           instance_variable_get(:"@#{name}")
