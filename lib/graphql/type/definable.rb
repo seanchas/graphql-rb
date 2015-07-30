6d7e468185fd8cc5e8fa794209a2800be1fa555b
Eugene Kovalev <seanchas@gmail.com>
2015-07-30 08:53:04 +0300

    Definable strategy

--- lib/graphql/type/definable.rb
+++ lib/graphql/type/definable.rb
@@ -3,18 +3,28 @@ module GraphQL
 
     module Definable
 
-      def attr_definable(name, proc)
+      def attr_definable(name, proc, strategy = :replace)
         @attr_definables ||= {} ; @attr_definables[name] = proc
 
         attr_reader name
+        ivar_name = :"@#{name}"
 
         define_method(name) do |*args|
           unless args.length == 0
             next_value = args.first
+            prev_value = instance_variable_get(ivar_name)
+
             raise GraphQL::Error::TypeError unless proc.call(next_value)
-            instance_variable_set(:"@#{name}", next_value)
+
+            next_value = case strategy
+            when :concat then (prev_value || []).concat(next_value)
+            when :merge then (prev_value || {}).merge(next_value)
+            else next_value
+            end
+
+            instance_variable_set(ivar_name, next_value)
           end
-          instance_variable_get(:"@#{name}")
+          instance_variable_get(ivar_name)
         end
       end
 
