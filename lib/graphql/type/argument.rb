28b2b699e1f1552cbc2eb9d20876b03cde18d072
Eugene Kovalev <seanchas@gmail.com>
2015-08-18 17:49:46 +0300

    Make GraphQLArgument a Configurable

--- lib/graphql/type/argument.rb
+++ lib/graphql/type/argument.rb
@@ -2,11 +2,15 @@ module GraphQL
 
   # GraphQLArgument
   #
-  class GraphQLArgument < GraphQL::Configuration::Base
+  class GraphQLArgumentConfiguration < GraphQL::Configuration::Base
     slot :name,           String, coerce: -> (v) { v.to_s }
     slot :type,           GraphQLInputType
     slot :default_value,  Object, null: true
     slot :description,    String, null: true
+  end
+
+  class GraphQLArgument < GraphQL::Configuration::Configurable
+    configure_with GraphQLArgumentConfiguration
   end
 
 end
