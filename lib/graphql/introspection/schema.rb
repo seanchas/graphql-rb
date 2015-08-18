8c9cef353af5edd93aecce0b27bf3686f1f45566
Eugene Kovalev <seanchas@gmail.com>
2015-08-18 17:19:00 +0300

    Modifications in Introspection Schema

--- lib/graphql/introspection/schema.rb
+++ lib/graphql/introspection/schema.rb
@@ -6,29 +6,26 @@ module GraphQL
 
       name '__Schema'
 
-      description   %(
-                      A GraphQL Schema defines the capabilities of a GraphQL server. It exposes all available types and directives on
-                      the server, as well as the entry points for query and mutation operations.
-                    )
+      description  'A GraphQL Schema defines the capabilities of a GraphQL server. It exposes all available types' +
+        'and directives on the server, as well as the entry points for query and mutation operations.'
 
       field :types, -> { ! + ! Type__ } do
         description 'A list of all types supported by this server.'
         resolve -> (schema) { schema.type_map.values }
       end
 
-      field :query_type, -> { ! Type__ } do
+      field :queryType, -> { ! Type__ } do
         description 'The type that query operations will be rooted at.'
         resolve -> (schema) { schema.query_type }
       end
 
-      field :mutation_type, -> { ! Type__ } do
+      field :mutationType, -> { ! Type__ } do
         description 'If this server supports mutation, the type that mutation operations will be rooted at.'
-        resolve -> (schema) { schema.query_type }
+        resolve -> (schema) { schema.mutation_type }
       end
 
       field :directives, -> { ! + ! Type__ } do
         description 'A list of all directives supported by this server.'
-        resolve -> (schema) { schema.directives }
       end
 
     end
@@ -37,16 +34,14 @@ module GraphQL
 
       name '__Directive'
 
-      field :name, ! GraphQLString
-      field :description, GraphQLString
+      field :name,          ! GraphQLString
+      field :description,     GraphQLString
 
-      field :args, -> { ! + ! InputValue__ } do
-        resolve -> (directive) { directive.args }
-      end
+      field :args,          -> { ! + ! InputValue__ }
 
-      field :on_operation,  ! GraphQLBoolean
-      field :on_fragment,   ! GraphQLBoolean
-      field :on_field,      ! GraphQLBoolean
+      field :onOperation,   ! GraphQLBoolean,  resolve: -> (directive) { directive.on_operation }
+      field :onFragment,    ! GraphQLBoolean,  resolve: -> (directive) { directive.on_fragment }
+      field :onField,       ! GraphQLBoolean,  resolve: -> (directive) { directive.on_field }
 
     end
 
@@ -73,12 +68,12 @@ module GraphQL
       end
 
       field :fields, -> { + ! Field__ } do
-        arg :include_deprecated, GraphQLBoolean, default_value: false
+        arg :includeDeprecated, GraphQLBoolean, default_value: false
 
-        resolve lambda { |type, params = {}|
+        resolve lambda { |type, params|
           return nil unless type.is_a?(GraphQLObjectType) || type.is_a?(GraphQLInterfaceType)
           fields = type.fields.values
-          fields = fields.select { |field| !field.deprecation_reason } unless params[:include_deprecated]
+          fields = fields.select { |field| !field.deprecation_reason } unless params[:includeDeprecated]
           fields
         }
       end
@@ -89,30 +84,31 @@ module GraphQL
         }
       end
 
-      field :possible_types, -> { + ! Type__ } do
+      field :possibleTypes, -> { + ! Type__ } do
         resolve lambda { |type|
           type.possible_types if type.is_a?(GraphQLInterfaceType) || type.is_a?(GraphQLUnionType)
         }
       end
 
-      field :enum_values, -> { + ! EnumValue__ } do
-        arg :include_deprecated, GraphQLBoolean, default_value: false
+      field :enumValues, -> { + ! EnumValue__ } do
 
-        resolve lambda { |type|
+        arg :includeDeprecated, GraphQLBoolean, default_value: false
+
+        resolve lambda { |type, params|
           return nil unless type.is_a?(GraphQLEnumType)
           values = type.values.values
-          values = values.select { |value| !value.deprecation_reason } unless params[:include_deprecated]
+          values = values.select { |value| !value.deprecation_reason } unless params[:includeDeprecated]
           values
         }
       end
 
-      field :input_fields, -> { + ! InputValue__ } do
+      field :inputFields, -> { + ! InputValue__ } do
         resolve lambda { |type|
           type.fields.values if type.is_a?(GraphQLInputObjectType)
         }
       end
 
-      field :of_type, -> { Type__ }
+      field :ofType, -> { Type__ }, resolve -> (type) { type.of_type rescue nil }
 
     end
 
@@ -121,10 +117,10 @@ module GraphQL
 
       field :name,              ! GraphQLString
       field :description,         GraphQLString
-      field :args,                -> { ! + ! InputValue__ }, resolve: -> (field) { field.args || [] }
+      field :args,                -> { ! + ! InputValue__ }
       field :type,                -> { ! Type__ }
-      field :is_deprecated,     ! GraphQLBoolean, resolve: -> (field) { !!field.deprecation_reason }
-      field :deprecation_reason,  GraphQLString
+      field :isDeprecated,      ! GraphQLBoolean,   resolve: -> (field) { !!field.deprecation_reason }
+      field :deprecationReason,   GraphQLString,    resolve: -> (field) { field.deprecation_reason }
     end
 
     InputValue__ = GraphQLObjectType.new do
@@ -133,7 +129,7 @@ module GraphQL
       field :name,          ! GraphQLString
       field :description,     GraphQLString
       field :type,            -> { ! Type__ }
-      field :default_value,   GraphQLString, resolve: -> (value) { value.default_value.nil? ? nil : raise("Not implemented. Yet.") }
+      field :defaultValue,   GraphQLString, resolve: -> (value) { value.default_value }
     end
 
     EnumValue__ = GraphQLObjectType.new do
@@ -141,8 +137,8 @@ module GraphQL
 
       field :name,              ! GraphQLString
       field :description,         GraphQLString
-      field :is_deprecated,     ! GraphQLBoolean, resolve: -> (value) { !!value.deprecation_reason }
-      field :deprecation_reason,  GraphQLString
+      field :isDeprecated,      ! GraphQLBoolean, resolve: -> (value) { !!value.deprecation_reason }
+      field :deprecationReason,   GraphQLString,  resolve: -> (value) { value.deprecation_reason }
     end
 
     TypeKind__ = GraphQLEnumType.new do
@@ -151,12 +147,12 @@ module GraphQL
 
       value :SCALAR,        :scalar,        'Indicates this type is a scalar.'
       value :OBJECT,        :object,        'Indicates this type is an object. `fields` and `interfaces` are valid fields.'
-      value :INTERFACE,     :interface,     'Indicates this type is an interface. `fields` and `possible_types` are valid fields.'
-      value :UNION,         :union,         'Indicates this type is a union. `possible_types` is a valid field.'
+      value :INTERFACE,     :interface,     'Indicates this type is an interface. `fields` and `possibleTypes` are valid fields.'
+      value :UNION,         :union,         'Indicates this type is a union. `possibleTypes` is a valid field.'
       value :ENUM,          :enum,          'Indicates this type is an enum. `values` is a valid field.'
       value :INPUT_OBJECT,  :input_object,  'Indicates this type is an input object. `fields` is a valid field.'
-      value :LIST,          :list,          'Indicates this type is a list. `of_type` is a valid field.'
-      value :NON_NULL,      :non_null,      'Indicates this type is a non-null. `of_type` is a valid field.'
+      value :LIST,          :list,          'Indicates this type is a list. `ofType` is a valid field.'
+      value :NON_NULL,      :non_null,      'Indicates this type is a non-null. `ofType` is a valid field.'
     end
   end
 
