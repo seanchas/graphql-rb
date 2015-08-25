8bfda7d75633d98c93d393e901fbe7db645f4e29
Eugene Kovalev <seanchas@gmail.com>
2015-08-25 21:45:01 +0300

    Use default value for input object field

--- lib/graphql/language/value.rb
+++ lib/graphql/language/value.rb
@@ -11,8 +11,10 @@ module GraphQL
           value.map { |value| value.materialize(type.of_type, variables) }
         when GraphQLInputObjectType
           type.fields.reduce({}) do |memo, field|
-            value_for_field = value.find { |v| v[:name] == field.name }[:value]
-            memo[field.name.to_sym] = value_for_field.materialize(field.type, variables)
+            value_for_field     = value.find { |v| v[:name] == field.name }[:value]
+            materialized_value  = value_for_field.materialize(field.type, variables)
+            materialized_value  = field.default_value if materialized_value.nil?
+            memo[field.name.to_sym] = materialized_value unless materialized_value.nil?
             memo
           end
         when GraphQLScalarType, GraphQLEnumType
